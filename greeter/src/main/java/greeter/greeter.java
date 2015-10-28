e8c368162647dd8b8cba7bd5e60ab394b19cf3dd
mehdi <mehdi@192.168.10.4>
2015-10-28 11:46:23 +0100

    MODYFING.GREETER.JAVA
    Signed-off-by: mehdi <mehdi@192.168.10.4>

--- greeter/src/main/java/greeter/greeter.java
+++ greeter/src/main/java/greeter/greeter.java
@@ -1,6 +1,17 @@
 package greeter;
 
 public class greeter {
+	
+	
+	
+	
+	/**
+	 * 
+	 * 
+	 * @nom+
+	 * @param nom
+	 * @return
+	 */
 	public String sayHello(String nom)
 	{
 		return "HELLO"+nom;
