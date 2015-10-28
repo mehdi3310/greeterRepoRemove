19b352f84622a6b22c4c8df73d1f96a8233aa693
mehdi <mehdi@192.168.10.4>
2015-10-28 10:36:06 +0100

    first comit singed-off-by:Mehdi.brnyoussef <mehdibenyoussefg1@gmail.com>

---
+++ greeter/src/main/java/greeter/greeter.java
@@ -0,0 +1,9 @@
+package greeter;
+
+public class greeter {
+	public String sayHello(String nom)
+	{
+		return "HELLO"+nom;
+	} 
+
+}
---
+++ greeter/src/test/java/edu.java
@@ -0,0 +1,14 @@
+
+import greeter.greeter;
+
+
+public class edu {
+	
+	public void itShouldsayHelloword(){
+		greeter greter =new greeter();
+		String result = greter .sayHello("world");
+		
+		
+
+}
+}
